16b4c655ac4d9263fc1efd8ad57ea46c06c362a5
Featherweight <vais.emma@gmail.com>
2019-10-23 16:42:22 -0400

    finished binary search specs

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,10 +1,27 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (array, target, head = 0, tail = array.length - 1) => {
+  // recursion until middle element returns true, or array has < 2
+  if (tail - head < 2) {
+    if (array[head] === target || array[tail] === target) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  // declare the element we're looking at
+  let currentIdx = Math.floor((tail + head) / 2);
+  if (array[currentIdx] === target) {
+    return true;
+  } else if (target < array[currentIdx]) {
+    // if target is smaller, half the array and call binary search on the left side
+    return binarySearch(array, target, head, currentIdx);
+  } else {
+    // if target is bigger, half the array and call binary search on the right side
+    return binarySearch(array, target, currentIdx, tail);
+  }
 };
-
 /*
 	EXTRA CREDIT:
 
@@ -14,4 +31,6 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+// this is for git
+
+module.exports = binarySearch;
